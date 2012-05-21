5c05a82f402be00a2ff2e0baa7c0a79b905bc32b
Adrian <adi@ottiker.com>
2012-05-21 17:33:48 +0200

    created main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+define(["./list"], function(List) {
+    
+    var Tab = {
+        
+        
+    };
+    
+    /*
+        config = {
+            
+            source: {
+                
+            },
+            bind: [
+                {
+                    
+                }
+            ]
+        }
+    */
+    
+    return function(config) {
+        
+        var tab = List(config);
+        
+        return tab;
+    };
+});
