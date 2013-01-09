e937d27f7d66696a76550d4f04044c0b906eba3f
Ionica Bizau <bizauionica@yahoo.com>
2013-01-09 16:44:20 +0200

    Added samples.

--- main.js
+++ main.js
@@ -1,89 +1,24 @@
 "use strict";
 
-define([], function () {
-    
-    /*
-        tabConf = {
-            
-            tabConainer: "#selector",
-            noTabSelected: "#selector",
-            itemTag: "li",
-            itemHTML: "<span></span>",
-            loadTabOnInit: {
-                    
-                tabId: "domains",
-                miid: "domain_manager"
-            },
-            source: {
-                name: "getData"
-            },
-            data: [{}, {}, ...]
-        }
-        
-        to create tabs that cotains a link use bind configuration options ex.:
-        bind = [{
-            val: "Tab Title",
-            query: "a",
-            tabId: "myId",
-            itemHTML: "<a href='http://myUrl.com'/>"
-        }]
-    */
-    var modCache = {};
-    
-    var Tab = {
-    
-        show: function (id, miid) {
-            
-            if (this.conf.tabs[id]) {
-                
-                for (var tab in this.conf.tabs) {
-                    
-                    if (tab !== id) {
-                    
-                        this.conf.tabs[tab].style.display = "none";
-                    }
-                }
-                
-                this.conf.noTabSelected.style.display = "none";
-                this.conf.tabs[id].style.display = "block";
-                
-                if (miid && !modCache[miid]) {
-                    
-                    modCache[miid] = 1;
-                    
-                    N.mod(this.conf.tabs[id], miid);
-                }
-            }
-        }
-    };
-    
-    function renderTabs(err, result) {
-        
-        if (!err) {
-            
-            var df = document.createDocumentFragment();
-            
-            for (var i = 0, l = result.length; i < l; ++i) {
-                
-                var elm = document.createElement(this.conf.tabTag);
-                
-                elm.setAttribute("id", result[i].tabId);
-                this.conf.tabs[result[i].tabId] = elm;
-                
-                df.appendChild(elm);
-                
-                if (this.conf.loadTabOnInit && this.conf.loadTabOnInit.tabId === result[i].tabId) {
-                    
-                    N.mod(elm, this.conf.loadTabOnInit.miid);
-                }
-            }
-            
-            this.conf.tabContainer.appendChild(df);
-        }
-    }
-    
+define([
+    "/jquery.js"
+], function () {
     function init(config) {
         var self = this;
+        
+        config.container = "#content";
+        config.tabs = ".nav-tabs li";
+        config.options = {};
+        config.options.classes = { selected: "active" }
+        
+        var container = $(config.container, self.dom);
+        
+        $(config.tabs, self.dom).on('click', function() {
+            $(config.tabs).removeClass(config.options.classes.selected);
+            $(config.container).html("");
+            $(this).attr("class", config.options.classes.selected);
+            M(config.container, $(this).attr("data-miid"));
+        });
     }
     
     return init;
