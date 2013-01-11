667d0b45e1dffdc1c61f91d25481e9a4b3c7884a
Ionica Bizau <bizauionica@yahoo.com>
2013-01-11 13:12:32 +0200

    Added the hash navigation feature.

--- main.js
+++ main.js
@@ -15,16 +15,42 @@ define([
         
         var container = $(config.container, self.dom);
         
-        // On page load
-        M(config.container, config.options.first);
+        // Load first content
+        if((config.options.first) && (!window.location.hash)) {
+            M(config.container, config.options.first);
+        }
+
+        // Load the right content for hash
+        if((window.location.hash) && (config.options.hash)) {
+            var tab = $("[data-hash=" + window.location.hash.substring(1) +  "]");
+            activateTab(config, tab);
+        }
         
         // On tab click
         $(config.tabs, self.dom).on('click', function() {
-            $(config.tabs).removeClass(config.options.classes.selected);
-            $(config.container).html("");
-            $(this).addClass(config.options.classes.selected);
-            M(config.container, $(this).attr("data-miid"));
+            activateTab(config, $(this));
+            
+            // Adds hash to URL
+            if (config.options.hash) {
+                window.location.hash = $(this).attr('data-hash');
+            }
+            
+            return false;
         });
+    }
+    
+    function activateTab(config, tab) {
+        // Removes the active class
+        $(config.tabs).removeClass(config.options.classes.selected);
+            
+        // Removes the content of container
+        $(config.container).html("");
+            
+        // Adds active class
+        tab.addClass(config.options.classes.selected);
+            
+        // Sets the content in container
+        M(config.container, tab.attr("data-miid"));
     }
     
     function buildSampleConfiguration(config) {
@@ -33,6 +59,7 @@ define([
         config.options = {};
         config.options.classes = { selected: "active" };
         config.options.first = "tabContent1";
+        config.options.hash = true;
     }
     
     return init;
